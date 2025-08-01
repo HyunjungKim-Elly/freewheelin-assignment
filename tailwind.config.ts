9969c82b0689c8d7104ab09c3c04d3ef80da66b5
Hyunjung Kim <ujujo@naver.com>
2025-08-01 21:42:22 +0900

    re-init: flatten project structure

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            screens: {
+                lg: '1024px',
+                xl: '1280px',
+            },
+        },
+    },
+    plugins: [],
+}
+
+export default config
