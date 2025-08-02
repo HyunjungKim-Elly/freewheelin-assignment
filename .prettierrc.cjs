098a8d0f3bd6d20af4c5865c355162dfdbc12931
Hyunjung Kim <ujujo@naver.com>
2025-08-02 12:42:39 +0900

    prettier plugin적용

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 4,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
